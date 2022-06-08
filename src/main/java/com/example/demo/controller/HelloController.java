c0a596a43b9a6392ec0ea596e765ddd7b5578422
park.u <pyj0724@gmail.com>
2022-06-08 21:00:01 +0900

    첫 프로젝트

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller //컨트롤러 선언
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("hello")  // 스프링 부트 템플릿엔진 기본 viewName을 GET으로 넘어온 값과 매핑 resources:templates/{hello}.html
     public String hello(Model model){
-        model.addAttribute("data", "hello!");
-        return "hello";
+        model.addAttribute("data", "hello!");   //data 값을 hello!로 셋팅
+        return "hello"; //hello를 리턴
     }
 
 }
