26efa8987bb75b75a0c9eeaa256026ad27946733
park.u <pyj0724@gmail.com>
2022-06-15 21:00:50 +0900

    220615 회원 가입 및 테스트 케이스 작성 연습

---
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,50 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        //이름 중복 확인
+        //ctrl+alt+v = 'Optional<Member> byName =' 까지 자동생성
+        //ctrl+alt+위화살표+L = 줄정렬
+        //ctrl+alt+m 메서드로 소스코드 따로 분리
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     * */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+}
